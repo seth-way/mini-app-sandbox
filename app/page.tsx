aa735b6ddfd206c1ffe98be7cff42998460937c2
Seth Way <way.sethaniel@gmail.com>
2024-11-02 16:57:41 -0400

    remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
 import AppCard from "@/components/home/app-card";
-import { DEPLOY_URL } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
-import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
-import Image from "next/image";
-import { nFormatter } from "@/lib/utils";
 
 export default function Home() {
   return (
